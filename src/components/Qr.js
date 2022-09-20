193714bb0254bf707b7e855f61ab4baf75d27d4e
Imtiaz Nayeem Shawon <imtiaznayim95@gmail.com>
2022-09-20 19:29:35 -0400

    qr scanner testing

--- src/components/Qr.js
+++ src/components/Qr.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-scanner'
+import { React, useState } from "react";
+import { Fab, TextareaAutosize } from "@mui/material";
 
-class QrScanner  extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 100,
-      result: 'No result',
+import { Link } from "react-router-dom";
+import QrScan from "react-qr-scanner";
+
+function QRscanner() {
+  const [qrscan, setQrscan] = useState("No result");
+  const handleScan = (data) => {
+    if (data) {
+      setQrscan(data);
     }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
 
-    this.handleScan = this.handleScan.bind(this)
-  }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
-  }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    const previewStyle = {
-      height: 240,
-      width: 320,
-    }
+  return (
+    <div>
+      <span>QR Scanner</span>
 
-    return(
-      <div>
-        <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
+      <center>
+        <div style={{ marginTop: 30 }}>
+          <QrScan
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ height: 240, width: 320 }}
           />
-        <p>{this.state.result}</p>
-      </div>
-    )
-  }
+        </div>
+      </center>
+
+      <TextareaAutosize
+        style={{ fontSize: 18, width: 320, height: 100, marginTop: 100 }}
+        rowsMax={4}
+        defaultValue={qrscan}
+        value={qrscan}
+      />
+    </div>
+  );
 }
-export default QrScanner ;
+
+export default QRscanner;
